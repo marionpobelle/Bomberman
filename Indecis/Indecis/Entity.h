277b598d98f770724a6570205c597f9f66d42cd4
Alvina <alvinadr24@gmail.com>
2023-09-20 14:47:22 +0200

    fix: files appearing in visual

--- Indecis/Indecis/Entity.h
+++ Indecis/Indecis/Entity.h
@@ -1,10 +1,12 @@
+#pragma once
+
 class Entity
 {
 public:
     float x;
     float y;
 
-    Entity(int _x, int _y) {     // Constructor
+    Entity(float _x, float _y) {     // Constructor
         x = _x;
         y = _y;
     }
