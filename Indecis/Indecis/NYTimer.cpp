99ef5f52f46327f1eb0305b72b0d41c5fd990851
Alvina <alvinadr24@gmail.com>
2023-09-21 15:14:22 +0200

    timer working

--- Indecis/Indecis/NYTimer.cpp
+++ Indecis/Indecis/NYTimer.cpp
@@ -1,1 +1,39 @@
 #include "NYTimer.h"
+ LARGE_INTEGER NYTimer::lastUpdateTime;
+ LONGLONG NYTimer::freq;
+
+NYTimer::NYTimer() { //constructor
+	QueryPerformanceCounter(&lastUpdateTime);
+	LARGE_INTEGER li_freq;
+	QueryPerformanceFrequency(&li_freq);
+	freq = li_freq.QuadPart;
+	freq /= 1000;
+}
+
+void NYTimer::Start(void) {
+	QueryPerformanceCounter(&lastUpdateTime);
+}
+
+float NYTimer::getElapsedSeconds(bool restart) {
+	LARGE_INTEGER timeNow;
+	QueryPerformanceCounter(&timeNow);
+	LONGLONG elapsedLong = timeNow.QuadPart - lastUpdateTime.QuadPart;
+
+	float elapsed = (float)((float)elapsedLong / (float)freq);
+	elapsed /= 1000.0f;
+
+	if (restart)
+		lastUpdateTime = timeNow;
+
+	return elapsed;
+}
+
+unsigned long NYTimer::getElapsedMs(bool restart)
+{
+	LARGE_INTEGER timeNow;
+	QueryPerformanceCounter(&timeNow);
+	LONGLONG elapsedLong = timeNow.QuadPart - lastUpdateTime.QuadPart;
+
+	unsigned long elapsed = (unsigned long)((float)elapsedLong / (float)freq);
+	return elapsed;
+}
