335c601a84f0a55bf82f7664b5cba241bc792fa8
Marion Pobelle <marion.pobelle@hotmail.com>
2023-10-18 13:46:01 +0200

    Documentation Sprite & SpriteReader

--- Indecis/Indecis/SpriteReader.cpp
+++ Indecis/Indecis/SpriteReader.cpp
@@ -5,41 +5,37 @@
 
 std::vector<Sprite*> SpriteReader::spriteList;
 
-SpriteReader::SpriteReader() { //constructor
+SpriteReader::SpriteReader() {
 
     //Load all references in specific folder and assign them a name
     std::string path = "sprites";
-    for (const auto& file : std::filesystem::directory_iterator(path)) {
-        ReadAndSaveSprite(file.path().string());
+    for ( const auto& file : std::filesystem::directory_iterator( path ) ) {
+        ReadAndSaveSprite( file.path().string() );
     }
 }
 
-void SpriteReader::ReadAndSaveSprite(std::string fileName) {
-    //Recuperer le contenu du fichier, regarde quel type d'objet c'est et affiche le char
-    //dans le buffer en consequence
+void SpriteReader::ReadAndSaveSprite( std::string fileName ) {
     std::ifstream input;
-    input.open(fileName);
+    input.open( fileName );
     int tileCompositionMinSize = 3;
 
     std::stringstream strStream;
     strStream << input.rdbuf(); //read the file
     std::string value = strStream.str(); //str holds the content of the file
-    value.erase(std::remove(value.begin(), value.end(), '\n'), value.cend());
+    value.erase( std::remove( value.begin(), value.end(), '\n' ), value.cend() );
 
     input.close();
 
-    spriteList.push_back(new Sprite(fileName, value));
+    spriteList.push_back( new Sprite( fileName, value ) );
 }
 
-std::string SpriteReader::CallSprite(std::string spriteName) {
+std::string SpriteReader::CallSprite( std::string spriteName ) {
     std::string returnValue = " ";
-    for (size_t i = 0; i < SpriteReader::spriteList.size(); i++)
+    for ( size_t i = 0; i < SpriteReader::spriteList.size(); i++ )
     {
         std::string path = "sprites\\" + spriteName + ".txt";
-        if (SpriteReader::spriteList[i]->name == path) {
-            //std::cout << SpriteReader::spriteList[i]->name << std::endl;
+        if ( SpriteReader::spriteList[i]->name == path ) {
             returnValue = SpriteReader::spriteList[i]->sprite;
-            //std::cout << returnValue << std::endl;
         }
     }
     return returnValue;
--- Indecis/Indecis/SpriteReader.h
+++ Indecis/Indecis/SpriteReader.h
@@ -10,12 +10,42 @@
 
 class Sprite;
 
+/*
+===============================================================================
+The SpriteReader class characterize the methods allowing the use of Sprites.
+===============================================================================
+*/
 class SpriteReader
 {
 public:
+	/*
+	====================
+	SpriteReader()
+		Constructor for the SpriteReader object.
+	====================
+	*/
     SpriteReader();
-    static void ReadAndSaveSprite(std::string fileName);
-    static std::string CallSprite(std::string spriteName);
+
+	/*
+	====================
+	ReadAndSaveSprite()
+		Read a sprite value from a fie and add it to the sprite list for the game.
+		@param fileName : String that defines the name of the file that contains the sprite.
+	====================
+	*/
+    static void ReadAndSaveSprite( std::string fileName );
+
+	/*
+	====================
+	CallSprite()
+		Get a sprite from the sprite list of the game.
+		@param spriteName : Name of the sprite to get from the sprite list.
+		@return : String, the sprite we wanted to get.
+	====================
+	*/
+    static std::string CallSprite( std::string spriteName );
+
+	//Sprite list of the game.
     static std::vector<Sprite*> spriteList;
 };
 
